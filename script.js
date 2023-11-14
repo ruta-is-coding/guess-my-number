641775164ab088df4447ecd7b2f127bb977bf404
ruta-is-coding <rutajurg.123@gmail.com>
2023-11-14 15:23:14 +0200

    Refactored my code (made it cleaner)

--- script.js
+++ script.js
@@ -1,51 +1,53 @@
 'use strict';
+//Generate a secret number
+const displaySecretNumber = value =>
+  (document.querySelector('.number').textContent = value);
+const generateSecretNumber = () => Math.trunc(Math.random() * 20) + 1;
+
 //Defining the variables
-let secretNumber = Math.trunc(Math.random() * 20) + 1;
+let secretNumber = generateSecretNumber();
 let score = 20;
 let highscore = 0;
+let input = document.querySelector('.guess');
 
-/*In the addEventListener method the first argument is an event name
-The second argument is a function value */
+//Other functions
+const displayMessage = message =>
+  (document.querySelector('.message').textContent = message);
+const displayScore = score =>
+  (document.querySelector('.score').textContent = score);
+const changeStyle = (color, width) => {
+  document.querySelector('body').style.backgroundColor = color;
+  document.querySelector('.number').style.width = width;
+};
+
+//When we click check button
 document.querySelector('.check').addEventListener('click', function () {
-  //We get a string from an input field (not a number), so we need to convert it to a number
-  const guess = Number(document.querySelector('.guess').value);
+  //Converting the input value to a number
+  const guess = Number(input.value);
 
-  /*Checking whether there's a value in the input field
-  When there's no input value*/
+  //When there's no input value
   if (!guess) {
-    document.querySelector('.message').textContent = '😐 No number';
+    displayMessage('😐 No number');
 
     //When the guess is correct
   } else if (guess === secretNumber) {
-    document.querySelector('.message').textContent = ' 🤩 Correct guess';
-    document.querySelector('.number').textContent = secretNumber;
-    document.querySelector('body').style.backgroundColor = '#60b347';
-    document.querySelector('.number').style.width = '30rem';
+    displayMessage('🤩 Correct guess');
+    displaySecretNumber(secretNumber);
+    changeStyle('#60b347', '30rem');
     if (score > highscore) {
       highscore = score;
       document.querySelector('.highscore').textContent = highscore;
     }
 
-    //When the guess is too high
-  } else if (guess > secretNumber) {
+    //When the guess is wrong
+  } else if (guess !== secretNumber) {
     if (score > 1) {
-      document.querySelector('.message').textContent = '😐 Too high';
+      displayMessage(guess > secretNumber ? '😐 Too high' : '😐 Too low');
       score--;
-      document.querySelector('.score').textContent = score;
+      displayScore(score);
     } else {
-      document.querySelector('.message').textContent = '😪 You lost the game';
-      document.querySelector('.score').textContent = 0;
-    }
-
-    //When the guess is too low
-  } else if (guess < secretNumber) {
-    if (score > 1) {
-      document.querySelector('.message').textContent = '😐 Too low';
-      score--;
-      document.querySelector('.score').textContent = score;
-    } else {
-      document.querySelector('.message').textContent = '😪 You lost the game';
-      document.querySelector('.score').textContent = 0;
+      displayMessage('😪 You lost the game');
+      displayScore('0');
     }
   }
 });
@@ -53,11 +55,10 @@ document.querySelector('.check').addEventListener('click', function () {
 //When we click again button
 document.querySelector('.again').addEventListener('click', function () {
   score = 20;
-  secretNumber = Math.trunc(Math.random() * 20) + 1;
-  document.querySelector('.message').textContent = 'Start guessing...';
-  document.querySelector('.number').textContent = '?';
-  document.querySelector('.score').textContent = score;
-  document.querySelector('.guess').value = '';
-  document.querySelector('body').style.backgroundColor = 'rgb(94, 94, 94)';
-  document.querySelector('.number').style.width = '15rem';
+  secretNumber = generateSecretNumber();
+  displayMessage('Start guessing...');
+  displaySecretNumber('?');
+  displayScore(score);
+  changeStyle('rgb(94, 94, 94)', '15rem');
+  input.value = '';
 });
